dfad4d39c7a3911c55ee6bb51b4c1f8bb734d442
k-masatany <masatani@fusic.co.jp>
2020-12-17 22:01:08 +0900

    カートリッジの写経

--- src/bus.rs
+++ src/bus.rs
@@ -1,4 +1,5 @@
 use crate::cpu::Mem;
+use crate::cartridge::Rom;
 
 //  _______________ $10000  _______________
 // | PRG-ROM       |       |               |
@@ -34,13 +35,24 @@ const PPU_REGISTERS_MIRRORS_END: u16 = 0x3FFF;
 
 pub struct Bus {
     cpu_vram: [u8; 2048],
+    rom: Rom,
 }
 
 impl Bus {
-    pub fn new() -> Self {
+    pub fn new(rom: Rom) -> Self {
         Bus {
             cpu_vram: [0; 2048],
+            rom: rom,
         }
+    }
+
+    fn read_prg_rom(&self, mut addr: u16) -> u8 {
+        addr -= 0x8000;
+        if self.rom.prg_rom.len() == 0x4000 && addr >= 0x4000 {
+            //mirror if needed
+            addr = addr % 0x4000;
+        }
+        self.rom.prg_rom[addr as usize]
     }
 }
 
@@ -55,6 +67,8 @@ impl Mem for Bus {
                 let _mirror_down_addr = addr & 0b00100000_00000111;
                 todo!("PPU is not supported yet")
             }
+            0x8000..=0xFFFF => self.read_prg_rom(addr),
+
             _ => {
                 println!("Ignoring mem access at {}", addr);
                 0
@@ -72,6 +86,10 @@ impl Mem for Bus {
                 let _mirror_down_addr = addr & 0b00100000_00000111;
                 todo!("PPU is not supported yet");
             }
+            0x8000..=0xFFFF => {
+                panic!("Attempt to write to Cartridge ROM space")
+            }
+
             _ => {
                 println!("Ignoring mem write-access at {}", addr);
             }
